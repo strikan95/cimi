e325ffd9b075052eae8e2b363f268b4978d0e988
Strikan <buljevic.juraj@gmail.com>
2024-07-04 14:21:35 +0200

    fixture fix

--- src/Shared/DataFixtures/UserFixture.php
+++ src/Shared/DataFixtures/UserFixture.php
@@ -11,20 +11,19 @@ class UserFixture extends BaseFixture
     {
         // "password" hashed -> $2y$13$IsHt6.rtUY522b6vFFozju5RBS1rF1Ud.eHO74Nc.Qn5ROMSDvUQ6
 
-
         $this->createMany(User::class, 300, function (User $user, $count) {
             $pass = "\$2y\$13\$7VVf9T3dHhjyabo0XWAbveCCiVDifM8P2WHuBf11YAPxXZKnWpX9.";
             if ($count == 0) {
                 $user->setEmail('juraj.buljevic@gmail.com');
                 $user->setPassword($pass);
-                $user->setRole('ROLE_HOST');
+                $user->setRoles('ROLE_HOST');
 
                 $user->setFirstName('Juraj');
                 $user->setLastName('Buljevic');
             } else {
                 $user->setEmail($this->faker->email());
                 $user->setPassword($pass);
-                $user->setRole('ROLE_HOST');
+                $user->setRoles('ROLE_HOST');
 
                 $user->setFirstName($this->faker->firstName());
                 $user->setLastName($this->faker->lastName());
